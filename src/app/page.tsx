ac982e7fb5b4dbdb0b9f50f2b24b703fcf7b0c4e
geopanthalassa <geopanthalassa@gmail.com>
2026-06-02 12:43:14 -0300

    fix: affiliates bar use iconColor for TypeScript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -236,7 +236,7 @@ export default async function Home() {
                                     {item.icon}
                                 </div>
                                 <div>
-                                    <div style={{ fontSize: '1rem', fontWeight: 900, color: item.color, letterSpacing: '-0.01em' }}>{item.label}</div>
+                                    <div style={{ fontSize: '1rem', fontWeight: 900, color: item.iconColor, letterSpacing: '-0.01em' }}>{item.label}</div>
                                     <div style={{ fontSize: '0.75rem', fontWeight: 600, color: item.iconColor }}>{item.sub}</div>
                                 </div>
                             </a>
@@ -481,6 +481,7 @@ export default async function Home() {
 }
 
  
+
 
 
 
